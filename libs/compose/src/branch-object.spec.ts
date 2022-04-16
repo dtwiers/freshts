33ed063dcd5c59f418ba996fc91dee5c8eeaa2ac
Derek Wiers <dtwiers08@gmail.com>
2022-04-16 13:37:11 -0500

    test(compose): fix branch-object test

--- libs/compose/src/branch-object.spec.ts
+++ libs/compose/src/branch-object.spec.ts
@@ -7,12 +7,16 @@ describe('branchObject', () => {
     FC.assert(
       FC.property(
         FC.anything(),
-        // TODO: make unique
-        FC.array(FC.tuple(FC.string(), FC.func(FC.anything()))),
+        FC.uniqueArray(FC.tuple(FC.string(), FC.func(FC.anything())), {
+          comparator: (a, b) => a[0] === b[0],
+        }),
         (param, branchEntries) => {
           const obj = Object.fromEntries(branchEntries);
           const result = pipe(param, branchObject(obj));
           expect(Object.keys(result)).toHaveLength(branchEntries.length);
+          branchEntries.forEach(([key, fn]) => {
+            expect(result[key] === fn(param));
+          });
         }
       )
     );
