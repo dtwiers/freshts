312576504ebb6ab3b89eca50daddf80e88a2ec0a
Derek Wiers <dtwiers08@gmail.com>
2022-05-30 10:17:44 -0500

    prototype Async action types

--- libs/state/builders/async/src/actions.types.ts
+++ libs/state/builders/async/src/actions.types.ts
@@ -1,4 +1,44 @@
 import { Action } from '@freshts/state-store';
 
-// TODO: Come up with a better type param name than ActionTypeType. That sounds stupid.
-export type AsyncAction<ActionTypeType extends string, PayloadType> = Action<{ type: ActionTypeType }, PayloadType>;
+export type ActionBehavior =
+  | 'append'
+  | 'replace'
+
+export type AsyncAction<
+  ActionBaseType extends string,
+  Subtype extends string,
+  PayloadType,
+  FilterMetadata,
+  PayloadMetadata
+> = Action<
+  {
+    type: `${ActionBaseType}:${Subtype}`;
+    meta: FilterMetadata;
+    behavior?: ActionBehavior;
+  },
+  {
+    data: PayloadType;
+    meta: PayloadMetadata;
+  }
+>;
+
+export type AsyncStartAction<
+  ActionBaseType extends string,
+  PayloadType,
+  FilterMetadata = void,
+  PayloadMetadata = void
+> = AsyncAction<ActionBaseType, 'Start', PayloadType, FilterMetadata, PayloadMetadata>;
+
+export type AsyncSucceedAction<
+  ActionBaseType extends string,
+  PayloadType,
+  FilterMetadata = void,
+  PayloadMetadata = void
+> = AsyncAction<ActionBaseType, 'Succeed', PayloadType, FilterMetadata, PayloadMetadata>;
+
+export type AsyncFailAction<
+  ActionBaseType extends string,
+  PayloadType,
+  FilterMetadata = void,
+  PayloadMetadata = void
+> = AsyncAction<ActionBaseType, 'Fail', PayloadType, FilterMetadata, PayloadMetadata>;
