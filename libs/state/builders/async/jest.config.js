187387e6d1fe5d53d70d539e75cbbc6667cf6f64
Derek Wiers <dtwiers08@gmail.com>
2022-06-22 10:18:42 -0500

    libs/state: fixed jest on state builders

--- libs/state/builders/async/jest.config.js
+++ libs/state/builders/async/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: 'state-async',
-  preset: '../../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/libs/state/async',
+  coverageDirectory: '../../../../coverage/libs/state/builders/async',
 };
--- libs/state/builders/entity/jest.config.js
+++ libs/state/builders/entity/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: 'state-entity',
-  preset: '../../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/libs/state/entity',
+  coverageDirectory: '../../../../coverage/libs/state/entity',
 };
