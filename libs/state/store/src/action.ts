21241bbc8784a92f1f917ee5763b8f0a6242776d
Derek Wiers <dtwiers08@gmail.com>
2022-07-07 18:40:44 -0500

    feat(state-store): allow for custom toString() in the action creator

--- libs/state/store/src/action.ts
+++ libs/state/store/src/action.ts
@@ -1,15 +1,19 @@
-import { ActionCreator, ActionCreatorOptions, HasTypeValue } from './action.types';
+import { Action, ActionCreator, ActionCreatorOptions, HasTypeValue } from './action.types';
 
 export const createActionCreator = <
   FilterType extends HasTypeValue<string>,
   PayloadType,
   CallbackInput extends unknown[] = [PayloadType]
 >(
-  options: ActionCreatorOptions<FilterType, PayloadType, CallbackInput>
+  options: ActionCreatorOptions<FilterType, PayloadType, CallbackInput>,
+  makeToString?: (action: Action<FilterType, PayloadType>) => string
 ): ActionCreator<FilterType, PayloadType, CallbackInput> => ({
   filter: options.filter,
   create: (...input) => ({
     filter: options.filter,
     payload: options.callback ? options.callback(...input) : (input[0] as PayloadType),
+    toString() {
+      return makeToString ? makeToString(this) : this.filter.type;
+    },
   }),
 });
